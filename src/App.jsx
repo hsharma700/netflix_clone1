544396226c3199826f25ec2a6d74e8d9db792182
Hsharma <himanshukrazzzy@gmail.com>
2022-11-01 21:35:36 +0530

    url changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import About from "./components/About";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="about" element={<About/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Navigate to="/" replace/>} />
       </Routes>
       <Footer />
     </div>
